83e7839e39ecec7a86270dc92b6449d1f9874d77
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-04-28 20:13:03 +0300

    Ready Settings
    Return Button works
    Mute Button doesn't
    MainManu Play Button works

---
+++ Witch on a Date/Assets/Scripts/Buttons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    // This function opens the SettingsScene when the SETTINGS button is clicked:
+    public void openSettings()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Loads next scene with build number x+1
+    }
+
+    // This function opens the playable game when the PLAY button is clicked:
+    public void openPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); // Loads next scene with build number x+2
+    }
+
+    // This function opens the MainMenu when RETURN button is pressed:
+    public void openMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); // Loads next scene with build number x-1
+    }
+}
