42c9e6ef11db05fea27b4c6fc4d208264fb819a3
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-05-30 19:08:50 +0300

    parallax
    ready parallax but not tested cuz I need camera script

---
+++ Witch on a Date/Assets/Scripts/Parallax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    // променливи:
+    private float length, startpos; // дължината и началната позиция на спрайта
+    public GameObject cam; // камерата
+    public float parallaxEffect; // стойност за прилагане на ефекта Parallax
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Как ще се променя картината на Fixed Update (Застопорено обновяване)
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect)); // temp е колко далеко съм пътувала спрямо (релативно на) камерата
+        float dist = (cam.transform.position.x * parallaxEffect); // dist е колко далеко съм стигнала в пространството на играта
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
