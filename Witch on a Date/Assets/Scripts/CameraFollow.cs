00c2147243227cfb9b98e0cb30e63491c2a4c15a
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-05-31 14:31:36 +0300

    CameraFollow
    Player moves up and down with keyboard and moves at a constant speed ahead. Camera Follows player. Parallax works

---
+++ Witch on a Date/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // обекта, който камерата следи
+    public Vector3 offset; // отгалечаване от целта
+
+    void FixedUpdate()
+    {
+        transform.position = target.position + offset; // следи целта
+    }
+}
---
+++ Witch on a Date/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f; // скорост на движение на играча
+    public Rigidbody2D rb; // Rigidbody2D на играча
+    Vector2 movement; // тук ще съхранявам User Input
+
+    /* Тук ще приемам Input сигналите */
+    void Update()
+    {
+        movement.x = moveSpeed; // константно движение по X със скорост moveSpeed
+        movement.y = Input.GetAxisRaw("Vertical"); // User Input по Y
+    }
+
+    /* Тук ще изпълнявам движенията на играча */
+    void FixedUpdate()
+    {
+        /* Time.fixedDeltaTime запазва moveSpeed константно. Това прави движението гладко.*/
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
