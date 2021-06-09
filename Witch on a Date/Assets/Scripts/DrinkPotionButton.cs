22b8595b189ba557dbaa9619eca81a1e89d9c1c3
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-06-09 18:14:27 +0300

    Mix Potion Ready
    No code to send data to next frame yet. Added second Canvas to the scene because the button was not clickable because I copied the Canvas from previous scene and the Event System bugged.

---
+++ Witch on a Date/Assets/Scripts/DrinkPotionButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DrinkPotionButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void drinkPotion()
+    {
+        // Отваря Result scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+}
---
+++ Witch on a Date/Assets/Scripts/PotionMixAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionMixAnimations : MonoBehaviour
+{
+    // Сегашно време
+    float currentTime = 0f;
+    // Задавам вектор среда на екрана
+    Vector3 temp1;
+
+    void Start()
+    {
+        temp1 = transform.position;
+        temp1.y = 0;
+        temp1.x = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Намалям времето с една секунда
+        currentTime += 1 * Time.deltaTime;
+
+        // Ако са изминали 6 секунди
+        if (currentTime >= 3 && currentTime <= 4)
+        {
+            // Показвам Smoke обекта, премествайки го на позиция (0, 0) което е средатата на екрана
+            transform.position = temp1;
+        }
+    }
+}
