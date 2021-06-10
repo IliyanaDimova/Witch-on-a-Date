9805fbad11213527390bae2cbb43797508685a38
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-06-10 16:11:56 +0300

    Flower Result Scene
    Also code for opening scenes from Potion Mix

--- Witch on a Date/Assets/Scripts/DrinkPotionButton.cs
+++ Witch on a Date/Assets/Scripts/DrinkPotionButton.cs
@@ -6,7 +6,29 @@ public class DrinkPotionButton : MonoBehaviour
     // Start is called before the first frame update
     public void drinkPotion()
     {
-        // Отваря Result scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4); 
+        // Ако има събрани 6 жаби, 3 цветя, 7 пеперуди
+        if (MixScoreManager.frogScore == 6 && MixScoreManager.butterflyScore == 3 && MixScoreManager.flowerScore == 7)
+        {
+            // Отваря Date сцена
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        // Ако в събраните съставки преобладават жаби
+        else if (MixScoreManager.frogScore > MixScoreManager.butterflyScore && MixScoreManager.frogScore > MixScoreManager.flowerScore)
+        {
+            // Отваря Frog Result сцена
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4); 
+        }
+        // Ако преобладават цветята
+        else if (MixScoreManager.flowerScore > MixScoreManager.frogScore && MixScoreManager.flowerScore > MixScoreManager.butterflyScore)
+        {
+            // Отваря Flower Result сцена
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        }
+        // Всички други случаи
+        else
+        {
+            // Отваря Butterfly Result сцена
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
     }
 }
