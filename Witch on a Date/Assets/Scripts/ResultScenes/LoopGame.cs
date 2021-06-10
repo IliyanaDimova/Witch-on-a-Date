ce40e25de0cfb2637b7cf79a865d0c68db118b20
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-06-10 17:51:35 +0300

    Loop Game
    Game returns to Main Menu when touch screen in Result Scene

---
+++ Witch on a Date/Assets/Scripts/ResultScenes/LoopGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoopGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Ако екрана е докоснат
+        if (Input.GetMouseButton(0))
+        {
+            // Отваря Main Menu сцена с индекс 0
+            SceneManager.LoadScene(0);
+        }
+    }
+}
