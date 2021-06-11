796797a879e0519dad07b6fb0e9c6ff0188e8ec0
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-06-11 17:45:14 +0300

    MainMenu ready
    Mute Button works and changes it's text in loop games. Exception is displayed - maybe a good idea to set val to mute when looping a game

---
+++ Witch on a Date/Assets/Scripts/MainMenuScene/Buttons.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Buttons : MonoBehaviour
+{
+    // Променлива в която сухранявам дали играта е със спрян звук
+    public static bool mute = false;
+    // Текста на Mute бутона
+    public TextMeshProUGUI changeText;
+
+    // Спира звука на играта
+    public void muteGame()
+    {
+        // Ако звука е спрян
+        if (mute)
+        {
+            // Смени текста на бутона на MUTE
+            changeText.text = "MUTE";
+        }
+        else
+        {
+            // Смени текста на бутона на UNMUTE
+            changeText.text = "UNMUTE";
+        }
+        // Смени състоянието на звука
+        mute = !mute;
+    }
+
+    // Отваря сцената PlayScene, когато бутонът PLAY е натиснат:
+    public void openPlay()
+    {
+        Sound.mute = mute;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Loads next scene with build number x+1
+    }
+
+}
---
+++ Witch on a Date/Assets/Scripts/MainMenuScene/MuteButtonText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class MuteButtonText : MonoBehaviour
+{
+    // Текста на Mute бутона
+    public TextMeshProUGUI changeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Buttons.mute)
+        {
+            // Смени текста на бутона на UNMUTE
+            changeText.text = "UNMUTE";
+        }
+    }
+}
