c446bcd0a205b8b135d0cad6e10b88aff4e23a21
Iliyana Dimova <iliyanadimova98@gmail.com>
2021-06-14 13:53:10 +0300

    Adds
    Ready game with Adds!

---
+++ Witch on a Date/Assets/Scripts/Adds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Adds : MonoBehaviour
+{
+    #if UNITY_IOS
+    string gameId = "4168942";
+    #else
+    string gameId = "4168943";
+    #endif
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(gameId);
+    }
+
+
+    public void PlayAd()
+    {
+        if (Advertisement.IsReady("video"))
+        {
+            Advertisement.Show("video");
+        }
+    }
+}
